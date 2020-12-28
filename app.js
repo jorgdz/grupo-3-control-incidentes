3aba1bdb0955276e7c546a7cfe6c5ee76b894c42
Jorge Diaz Montoya <jdzm@outlook.es>
2020-12-28 16:14:57 -0500

    Add content in landing page
    * Add content about services and contact
    * Add admin template

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const hbs = require('hbs')
 
-const indexRouter = require('./routes/index')
+var indexRouter = require('./routes/index')
+var adminRouter = require('./routes/admin')
 
 const app = express()
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 db.sequelize.sync()
 
 app.use('/', indexRouter)
+app.use('/admin', adminRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) { next(createError(404)) })
