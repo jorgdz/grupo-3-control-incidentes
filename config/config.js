1cefa1f56ce19363dc88e141d85a37294710269e
Jorge Diaz Montoya <jdzm@outlook.es>
2021-02-09 20:56:21 -0500

    test desploy 2

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: 'postgres',
-        timezone: '-05:00',
+        timezone: '+01:30',
         dialectOptions: {
             useUTC: false,
         },
