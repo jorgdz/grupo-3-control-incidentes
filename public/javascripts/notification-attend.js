fa23d3755abc39ba29804eb3f1f31a1102568cf7
Jorge Diaz Montoya <jdzm@outlook.es>
2021-02-17 03:03:26 -0500

    notify attend incident

---
+++ public/javascripts/notification-attend.js
@@ -0,0 +1,42 @@
+let countNotify = document.querySelector('#count_notify')
+let blockNotify = document.querySelector('#block-notify')
+
+const getUserAuth = async function () {
+  let url = '/valle-verde/api/auth/user'
+  const data = await fetch(url)
+  const user = data.json()
+  return user
+}
+
+window.addEventListener('load', function () {
+  let socket = io('https://incident-web.herokuapp.com', { 'forceNew': true })
+  //let socket = io('http://localhost:5000', { 'forceNew': true })
+  socket.on('data', async function (data) {
+    const user = await getUserAuth()
+
+    if (data.success == 2 && data.response.incidente.residente_id == user.residente.id) {
+      
+      // Notificación
+      countNotify.classList.remove('d-none')
+      countNotify.textContent = parseInt(countNotify.textContent) + 1
+
+      $('.nott-list').prepend(`
+        <div class="notfication-details">
+            <div class="noty-user-img">
+                <img src="${data.response.empleado.user.url_imagen}" alt="">
+            </div>
+            <div class="notification-info">
+                <h3><a href="/valle-verde/${data.response.incidente_id}/incidente" title="">${data.response.empleado.user.nombres} ${data.response.empleado.user.apellidos}</a> ${data.response.incidente.descripcion.substr(0, 34)}.</h3>
+                <br>
+                <span>${moment(new Date(data.response.createdAt), "YYYYMMDD").fromNow()}</span>
+            </div>
+        </div>
+      `)
+    }
+  })
+}, false)
+
+blockNotify.addEventListener('click', function () {
+  countNotify.classList.add('d-none')
+  countNotify.textContent = 0
+})
--- public/javascripts/personal-seguridad-incidentes.js
+++ public/javascripts/personal-seguridad-incidentes.js
@@ -18,19 +18,23 @@ let incidentContainer = $(".incidents-container");
 /** Listeners
 ==================================================================== */
 
-window.addEventListener('load', async () => {
+window.addEventListener('load', async function () {
 
   let incidents = await getIncidents();
   incidents.map(incident => {
     incidentContainer.append(getIncidentFromTemplate(incident))
+
+    attend(incident)
   })
 
-  let socket = io('https://incident-web.herokuapp.com', { 'forceNew': true })
-  //let socket = io('http://localhost:5000', { 'forceNew': true })
+  //let socket = io('https://incident-web.herokuapp.com', { 'forceNew': true })
+  let socket = io('http://localhost:5000', { 'forceNew': true })
 
   socket.on('data', async function (data) {
     if (data.success == 1) {
       incidentContainer.prepend(getIncidentFromTemplate(data.response))
+      
+      attend(data.response)
     }
   })
 
@@ -40,17 +44,56 @@ window.addEventListener('load', async () => {
 ==================================================================== */
 
 const getIncidents = async () => {
-  
   let url = '/valle-verde/api/incidentes/employee'
   const data = await fetch(url)
   const incidentes = data.json()
   return incidentes
+}
 
+const goAttend = async (id) => {
+  let url = `/valle-verde/api/${id}/incidentes/attention`
+  const data = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+
+  const attend = data.json()
+  return attend
+}
+
+const attend = function (incident) {
+  document.querySelector(`#attend-incident-${incident.id}`).addEventListener('click', function () {
+    goAttend (incident.id)
+      .then(response => {
+
+        console.log(response)
+        let socket = io('https://incident-web.herokuapp.com', { 'forceNew': true })
+        //let socket = io('http://localhost:5000', { 'forceNew': true })
+        socket.on('connect', async function () {
+          socket.emit('data', {
+            success: '2',
+            response: response
+          })
+        })
+        socket.on('disconnect', function () {})
+
+        document.querySelector('#totalAtenciones').textContent = parseInt(document.querySelector('#totalAtenciones').textContent) + 1
+        $(`#attend-incident-${incident.id}`).hide(300)
+
+      })
+  })
 }
 
 const getIncidentFromTemplate = (incident) => {
 
- return (`
+  let showBtnAttend = '';
+  if (incident.estado === 'NO ATENDIDO') {
+    showBtnAttend += `<a id="attend-incident-${incident.id}" href="#!" class="card-link">Atender</a>`
+  }
+
+  return (`
     <div class="card mt-4" style="width: 16rem;">
       <figure style="overflow: hidden";>
           <div class="gravedad bg-${SEVERITY[incident.tipo.gravedad]}">
@@ -60,25 +103,23 @@ const getIncidentFromTemplate = (incident) => {
           src="${incident.adjuntos.length == 0 ? DEFAULT_URL : incident.adjuntos[0].url}"
           alt="Card image cap" style="height: 180px; width: 286px"></figure>
       <div class="card-body">
-          <h5 class="card-title">
-            ${incident.residente.villa.bloque.nombre}: ${incident.residente.villa.numero}
-          </h5>
-          <p class="card-text">
-            <span class="d-inline-block">
-              ${incident.descripcion.substr(0, 34)}
-            </span>
+        <h5 class="card-title">
+          ${incident.residente.villa.bloque.nombre}: ${incident.residente.villa.numero}
+        </h5>
+        <p class="card-text">
+          <span class="d-inline-block">
+            ${incident.descripcion.substr(0, 34)}
+          </span>
         </p>
       </div>
       <ul class="list-group list-group-flush">
           <li class="list-group-item">${incident.tipo.tipo}</li>
-          
       </ul>
       <div class="card-body">
           <a href="/valle-verde/api/incidente/${incident.id}" class="card-link">Detalles</a>
-          <a id="attend-incident" href="/valle-verde/api/${incident.id}/incidentes/attention" onclick="event.preventDefault(); (confirm('¿ ATENDER ESTE INCIDENTE ?')) ? document.getElementById('attend-form-${incident.id}').submit() : false;" class="card-link">Atender</a>
-          <form id="attend-form-${incident.id}" action="/valle-verde/api/${incident.id}/incidentes/attention" method="POST" style="display: none;"></form>
+          ${showBtnAttend}
       </div>
-    </div>`
-    );
+  </div>`
+  );
 
 }
